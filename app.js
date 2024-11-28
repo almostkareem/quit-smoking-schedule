07ba1ed0df609428456892e27991380ece495274
almostkareem <24992264+almostkareem@users.noreply.github.com>
2024-11-28 11:23:37 +0100

    add reasons

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const twilio = require('twilio');
 const schedule = require('node-schedule');
+const fs = require('fs');
+
 
 // Twilio Credentials from Environment Variables
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -10,6 +12,20 @@ const timeZone = process.env.TIME_ZONE; // E.g., "Africa/Casablanca"
 // Your phone number and Twilio number
 const yourPhoneNumber = process.env.YOUR_PHONE_NUMBER; // E.g., "+212612345678"
 const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER; // E.g., "+1234567890"
+
+// Load reasons.csv file
+
+const reasons = fs.readFileSync('reasons.csv', 'utf8').split('\n').map((line) => line.trim()).filter((line) => line.length > 0);
+
+// Reason schedule
+
+const scheduleTimesWithReason = [
+    { hour: 10, minute: 0 },   // 10:00 AM
+    { hour: 13, minute: 0 },  // 1:00 PM
+    { hour: 16, minute: 0 },  // 4:00 PM
+    { hour: 19, minute: 0 },  // 7:00 PM
+    { hour: 22, minute: 0 }   // 10:00 PM
+];
 
 // Cigarette schedule
 
@@ -38,6 +54,30 @@ scheduleTimes.forEach(({ hour, minute }) => {
     schedule.scheduleJob({ hour, minute, tz: timeZone }, () => {
         const timeString = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
         sendSms(timeString);
+    });
+});
+
+// Function to send SMS with a random reason
+
+const sendSmsWithReason = (time) => {
+    
+    const message = `Friendly reminder why you should quit smoking:\n${reasons[Math.floor(Math.random() * reasons.length)]}`;
+    
+    client.messages.create({
+        body: message,
+        from: twilioPhoneNumber,
+        to: yourPhoneNumber,
+    })
+    .then((message) => console.log(`SMS sent: ${message.sid}`))
+    .catch((error) => console.error(`Error sending SMS: ${error}`));
+};
+
+// Schedule SMS with a random reason for each time
+
+scheduleTimesWithReason.forEach(({ hour, minute }) => {
+    schedule.scheduleJob({ hour, minute, tz: timeZone }, () => {
+        const timeString = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
+        sendSmsWithReason(timeString);
     });
 });
 
